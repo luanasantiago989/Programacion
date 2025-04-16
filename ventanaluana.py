8d5858ef81d40b8f4da15936ad7125cb0d73561b
luanasantiago989 <luanasantiago413@gmail.com>
2025-04-16 13:08:41 -0300

    Update ventanaluana2.py
    Clase 1 part 2

--- ventanaluana.py
+++ ventanaluana.py
@@ -18,16 +18,16 @@ def mostrarMenu():
      label = tk.Label(ventana, text="Menú Principal", font=("Arial", 18, "bold"))
      label.pack(pady=20)
  
-     boton1 = tk.Button(ventana, text="Opción 1", font=("Arial", 14), width=20, command=accion)
+     boton1 = tk.Button(ventana, text="Graficar funcion", font=("Arial", 14), width=20, command=accion)
      boton1.pack(pady=10)
  
-     boton2 = tk.Button(ventana, text="Opción 2", font=("Arial", 14), width=20, command=accion) 
+     boton2 = tk.Button(ventana, text="Graficar puntos especificos sobre la funcion", font=("Arial", 14), width=20, command=accion) 
      boton2.pack(pady=10)
 
-     boton3 = tk.Button(ventana, text="Opcion 3", font=("Arial",14),  whidth=20, command=accion)
+     boton3 = tk.Button(ventana, text="Encontrar raicesaproximada de la funcion", font=("Arial",14),  whidth=20, command=accion)
      boton3.pack(pady=10)
 
-     boton4 = tk.Button(ventana, text="Opcion 4", font=("Arial", 14), whidth=20, commamd=accion)
+     boton4 = tk.Button(ventana, text="Salir del programa", font=("Arial", 14), whidth=20, commamd=accion)
      boton4.pack(pady=10)
 
      def mostrar():
@@ -45,3 +45,6 @@ def mostrarMenu():
  
      boton_volver = tk.Button(ventana, text="Volver", font=("Arial", 12), command=mostrarMenu)
      boton_volver.pack(pady=10)
+
+def logritmica 
+
